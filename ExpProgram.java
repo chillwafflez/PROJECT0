74e68e69837606fdfff87e2b3f7d86af5a258e13
Justin Nguyen <ju5t1n.nguy3n284@gmail.com>
2022-01-29 03:24:07 -0800

    added java program

---
+++ ExpProgram.java
@@ -0,0 +1,26 @@
+
+//Justin Nguyen
+import java.util.Scanner;
+
+public class ExpProgram {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int first = keyboard.nextInt();
+        System.out.println("Enter a second number");
+        int second = keyboard.nextInt();
+
+        int remainder = first % second; // calculates divison and returns remainder
+
+        if (remainder == 0) { // if the first int is evenly divisible by the second int
+
+            System.out.println(first + " is evenly divisible by " + second);
+        } else // if the first int is not evenly divisible by the second int
+            System.out.println(
+                    first + " is not evenly divisible by " + second + " , the remainer is " + remainder);
+
+        keyboard.close();
+    } // end main
+} // end class
